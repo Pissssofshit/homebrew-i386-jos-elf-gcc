cdff192d2fa81d5a8c3c1ddee50a023c3d1ce7a8
Pissssofshit <1412143367@qq.com>
2017-09-12 23:12:04 +0800

    Update i386-jos-elf-gcc.rb

--- i386-jos-elf-gcc.rb
+++ i386-jos-elf-gcc.rb
@@ -1,7 +1,7 @@
 class I386JosElfGcc < Formula
   homepage "http://pdos.csail.mit.edu/6.828/2014/tools.html"
   url "http://mirrors.ustc.edu.cn/gnu/gcc/gcc-7.1.0/gcc-7.1.0.tar.bz2"
-  sha256 "0bbb8f754a31f29013f6e9ad4c755d92bb0f154a665c4b623e86ae7174d98e33"
+  sha256 "8a8136c235f64c6fef69cac0d73a46a1a09bb250776a050aec8f9fc880bebc17"
 
   depends_on 'gmp'
   depends_on 'libmpc'
